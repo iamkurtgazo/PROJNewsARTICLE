05f628fb27e6b69c5996fd6546c77f05c69488b0
Kurt Gazo <kurtgazo@gmail.comm>
2022-12-24 13:26:09 +0800

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import{ View,Text, ScrollView, TouchableOpacity} from "react-native";
+ import { useState,useEffect } from "react";
+ export default function App() {
+    const [article,setData]=useState([])
+   console.log(log);
+    const url = "https://newsapi.org/v2/everything?q=tesla&from=2022-11-11&sortBy=publishedAt&apiKey=bcf0cdfa39264ba5a8b28f9b4479237e"
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const getArticles= () =>{
+
+    fetch(url)
+      .then(res=>res.json())
+      .then((json)=>setData(json))
+      .catch((err)=>console.log(err))
+};
+  
+useEffect(() => {
+
+    getArticles();
+}, []) ;
+  return(
+    <> 
+    {
+      object.keys(Data).length > 0 &&(
+
+        <ScrollView>
+        <View
+          style={{
+            flex: 1,
+            justifyContent: "space around",
+            alignItems: "center"
+          }}
+        >
+          {
+            data.articles.map((article,index)=>(
+              <Card key={index}>
+                <View>
+                  style={{
+                    flexDirection:"row",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    marginVertical: 15
+                  }
+                  >
+                    <><Text style={{ fontSize: 18, fontWeight: "bold", flex: 1 }}>{article.title}</Text><TouchableOpacity>
+                      <Image
+                        source={{ uri: urlToImage }}
+                        style={{
+                          width: 150,
+                          height: 150,
+                          borderRadius: 10
+                        }} />
+                    </TouchableOpacity></>
+                  }</View>
+                  <Text style={{fontSize:16}}>{article.description}</Text>
+                    <Text>{article.descriptionAt}</Text>
+                  </Card>
+              ))  
+          }
+         </View>
+        </ScrollView>
+      )
+    }
+    </>
+  );    
+} 
